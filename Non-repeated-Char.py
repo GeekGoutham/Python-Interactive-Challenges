21bfb52c96c83a2c8601f6edbacf1a61bc04b885
Vignesh Goutham Ganesh <geekgoutham@gmail.com>
2016-03-21 14:30:07 -0700

    Adding v2 (using string.count) and comments

--- Non-repeated-Char.py
+++ Non-repeated-Char.py
@@ -1,6 +1,6 @@
 #Author = Vignesh Goutham
 
-def getNonRepeatChar(string1):
+def getNonRepeatChar(string1):              #Both algorithms have the same time complexity of O(n), but the lower v2 looks clean and might be having lesser space complexity
     chardict = {}
     for ch in string1:
         if ch in chardict.keys():
@@ -16,5 +16,15 @@ def getNonRepeatChar(string1):
     print("No characters are non-repeated in the given string")
 
 
+def getNonRepeatChar_v2(string1):
+    for ch in string1:
+        count = string1.count(ch)
+        if count == 1:
+            print(ch + " is the non-repeating first char")
+            return
+
+
+
+
 if __name__ == "__main__":
-    getNonRepeatChar("vignesh")
+    getNonRepeatChar_v2("vsngivignesh")
