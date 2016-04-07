7b0942278637768bec55898678840e83ac12f715
Vignesh Goutham Ganesh <geekgoutham@gmail.com>
2016-04-07 12:01:51 -0700

    Tried using xor for getting the odd char in string

--- Non-repeated-Char.py
+++ Non-repeated-Char.py
@@ -1,7 +1,7 @@
 #Author = Vignesh Goutham
 
 def getNonRepeatChar(string1):              #The bottom algorithm is more good
-    chardict = {}                           #Reading and witing to dict is O(n) -- so algorithm might be O(n^2)
+    chardict = {}                           #Reading and witing to dict is O(1), but dict.keys() is O(n) -- so algorithm might be O(n^2)
     for ch in string1:
         if ch in chardict.keys():
             chardict[ch] += 1
@@ -23,8 +23,16 @@ def getNonRepeatChar_v2(string1):
             print(ch + " is the non-repeating first char")
             return
 
-
+def getNonRepeatChar_v3(string1):           #not working properly with the xor -- but worked for numbers (integers)
+    result = 0
+    asc = [ord(ch) for ch in string1]
+    print(asc)
+    for ch in asc:
+        result ^= ch
+        print(result)
+    res = chr(result)
+    print(res)
 
 
 if __name__ == "__main__":
-    getNonRepeatChar_v2("vsngivignesh")
+    getNonRepeatChar_v3("vingvighnesh")
